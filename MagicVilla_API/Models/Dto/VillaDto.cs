6a188786cfe57b2fa389a9886761fc09162ea834
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-04-25 11:07:23 -0500

    Add: EntityFramework Nuget packages

--- MagicVilla_API/Models/Dto/VillaDto.cs
+++ MagicVilla_API/Models/Dto/VillaDto.cs
@@ -9,8 +9,14 @@ namespace MagicVilla_API.Models.Dto
         [MaxLength(30)]
         public string Nombre { get; set; }
 
+        public string Detalle { get; set; }
+        [Required]
+        public double Tarifa { get; set; }
+
         public int Ocupantes { get; set; }
 
         public int MetrosCuadrados { get; set;}
+        public string ImagenUrl { get; set; }
+        public string Amenidad { get; set; }
     }
 }
