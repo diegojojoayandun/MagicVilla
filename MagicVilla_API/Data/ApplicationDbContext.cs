16a19f8d26638b0a450a98ed7c34610b495f3997
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-04-25 11:30:32 -0500

    Add: migrate and update database from EFCore

--- MagicVilla_API/Data/ApplicationDbContext.cs
+++ MagicVilla_API/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace MagicVilla_API.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
         public DbSet<Villa> Villas{ get; set; }
     }
 }
---
+++ MagicVilla_API/Migrations/20230425162558_AddDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_API.Migrations
+{
+    public partial class AddDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Villas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Detalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tarifa = table.Column<double>(type: "float", nullable: false),
+                    Ocupantes = table.Column<int>(type: "int", nullable: false),
+                    MetrosCuadrados = table.Column<int>(type: "int", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amenidad = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCreacíon = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Villas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Villas");
+        }
+    }
+}
