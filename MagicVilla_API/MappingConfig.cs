97e3a6c28dfd41dd9f33c5ecb2ea02b7bc83e28b
Diego Fernando Jojoa <91098381+diegojojoayandun@users.noreply.github.com>
2023-04-27 09:36:43 -0500

    ADD: add automapper NuGet package and class to mapping

---
+++ MagicVilla_API/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Villa, VillaDto>();
+            CreateMap<VillaDto, Villa>();
+
+            CreateMap<Villa, VillaCreateDto>().ReverseMap(); // This is equal to previuos code , but ... in one line
+            CreateMap<Villa, VillaUpdateDto>().ReverseMap();
+
+        }
+    }
+}
